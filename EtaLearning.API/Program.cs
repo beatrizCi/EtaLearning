0aea733897971c5b4c4b2a08e2739a5db36d42a0
Olga Beatriz Fernando da Silva <olga.beatriz@etaplus.energy>
2023-09-21 12:41:53 +0200

    moving startup code into program.cs

--- EtaLearning.API/Program.cs
+++ EtaLearning.API/Program.cs
@@ -22,4 +22,15 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Clients.AddRange(
+               new Clients { Id = 1, Name = "Lustitia Ltd", CreationDate = DateTime.UtcNow },
+               new Clients { Id = 2, Name = "Bachmann", CreationDate = DateTime.UtcNow }
+           );
+    dbContext.SaveChanges();
+}
+
 app.Run();
